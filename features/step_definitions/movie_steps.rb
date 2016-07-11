a9c1a56abd5864a4c144083423380816007183be
Kevin Story <kevinjstory@live.com>
2016-07-11 13:38:46 +0000

    Added step defintions

--- features/step_definitions/movie_steps.rb
+++ features/step_definitions/movie_steps.rb
@@ -12,3 +12,25 @@ Then /I should see "(.*)" before "(.*)" on (.*)/ do |string1, string2, path|
   regexp = /#{string1}.*#{string2}/m #  /m means match across newlines
   page.body.should =~ regexp
 end
+
+
+
+Given /the following movies exist/ do |movies_table|
+  movies_table.hashes.each do |movie|
+    Movie.create movie
+  end
+end
+
+# Make sure that one string (regexp) occurs before or after another one
+#   on the same page
+
+Then /I should see "(.*)" before "(.*)"/ do |e1, e2|
+  assert page.body =~ /#{e1}.*#{e2}/m, "#{e1} was not before #{e2}"
+end
+
+
+Then /^the director of "(.*?)" should be "(.*?)"$/ do |movie_title, new_director|
+  movie = Movie.find_by_title movie_title
+  movie.director.should == new_director
+end
+
