417939e62f8383dc21c6ab678fa58513b74b7ce5
Kevin Story <kevinjstory@live.com>
2016-07-11 13:49:22 +0000

    Added find same director path

--- features/support/paths.rb
+++ features/support/paths.rb
@@ -21,7 +21,12 @@ module NavigationHelpers
       '/movies/new'
     when /^the edit page for "(.*)"$/
     	edit_movie_path Movie.find_by_title($1)
-
+    	
+    when /^the details page for "(.*)"$/
+    	movie_path Movie.find_by_title($1)	
+    	
+    when /^the Similar Movies page for "(.*)"$/
+      	same_director_path Movie.find_by_title($1)
     # Add more mappings here.
     # Here is an example that pulls values out of the Regexp:
     #
